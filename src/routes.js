8ee0158ce62c8537e39bc6da976cfea161bf9b5b
Jonatas Venancio <49959786+jovenan@users.noreply.github.com>
2021-07-25 01:16:37 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
 import Register from './Pages/Register';
@@ -7,13 +7,13 @@ import Login from './Pages/Login';
 
 export default function Routes() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Home} />
                 <Route path='/cart' exact component={Cart} />
                 <Route path='/cadastrar' exact component={Register} />
                 <Route path='/login' exact component={Login} />
             </Switch>
-        </HashRouter>
+        </BrowserRouter>
     )
 }
