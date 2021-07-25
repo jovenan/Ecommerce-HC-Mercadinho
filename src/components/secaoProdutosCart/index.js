3d3e64047504287e1ccc8c6fdaed8df87a11a765
Jonatas Venancio <49959786+jovenan@users.noreply.github.com>
2021-07-25 01:50:25 -0300

    Update index.js

--- src/components/secaoProdutosCart/index.js
+++ src/components/secaoProdutosCart/index.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
+import React, { useEffect, useState } from "react";
 import * as S from './styled';
 
 export default function App() {
