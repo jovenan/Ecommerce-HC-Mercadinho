d5a2306d3388cd075d485c21bec2a2ab1caaec87
Jonatas Venancio <49959786+jovenan@users.noreply.github.com>
2021-07-25 01:13:11 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom';
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
 import Register from './Pages/Register';
